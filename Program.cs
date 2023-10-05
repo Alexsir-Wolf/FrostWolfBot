ba7e03108a87d0cff49d68841852899ecca37fe5
Alex Freitas <freitas.alex.soares@outlook.com>
2023-10-05 00:28:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using FrostWolfBot.config;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace FrostWolfBot
+{
+	public class Program
+	{
+        private static DiscordClient Cliente { get; set; }
+        private static CommandsNextExtension Commandos { get; set; }
+
+        static async Task Main(string[] args)
+		{
+			var lerJson = new configJSON();
+			await lerJson.LerJSON();
+
+			var discordConfig = new DiscordConfiguration()
+			{
+				Intents = DiscordIntents.All,
+				Token = lerJson.Token,
+				TokenType = TokenType.Bot,
+				AutoReconnect = true,
+			};
+
+			Cliente = new DiscordClient(discordConfig);
+			Cliente.Ready += Cliente_Ready;
+
+			await Cliente.ConnectAsync();
+			await Task.Delay(-1);
+		}
+
+		private static Task Cliente_Ready(DiscordClient sender, ReadyEventArgs args)
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ config/configJSON.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FrostWolfBot.config
+{
+	public class configJSON
+	{
+        public string Token { get; set; }
+        public string Prefix { get; set; }
+
+        public async Task LerJSON()
+        {
+            using(StreamReader SR = new StreamReader("config.json"))
+            {
+                string json = await SR.ReadToEndAsync();
+                StruturaJSON data = JsonConvert.DeserializeObject<StruturaJSON>(json);
+
+                this.Token = data.Token;
+                this.Prefix = data.Prefix;
+            }
+        }
+    }
+
+    internal sealed class StruturaJSON
+    {
+        public string Token { get; set; }
+        public string Prefix { get; set; }
+        
+    }
+}
